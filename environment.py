5d48ff6193124eadcedeb6b558cf578ba2f72921
kazami <kazamizero@gmail.com>
2018-03-25 10:43:57 +0800

    1. fix random gen and btach size more than 1

--- environment.py
+++ environment.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 import subprocess
+import random
 import os
+import struct
+import parse
 
 
 class Environment(object):
@@ -29,4 +32,122 @@ class Environment(object):
       process.wait()
       print "process finish"
 
+class RandomPlayer:
+  def __init__(self, sock):
+    self._iter = 1
+    self._p = subprocess.Popen(['../llvm-reg/llvm/build/bin/llc', '-debug-only=regallocdl', '--regalloc=drl', 'add.ll', '-o', 'convba.s'],shell=False, stdout=subprocess.PIPE)
+    self._sock = sock
+    self._sock.listen(5)
 
+  def interact(self):
+    terminal = False
+    actions = set()
+    while (terminal == False):
+      print "start accept " + str(self._iter)
+      conn, addr = self._sock.accept()
+      data = conn.recv(1024)
+      if (data[0] == 'e'):
+        terminal = True
+        break
+      reward_map = self.getState(data)
+      action = self.doAction(reward_map)
+      conn.send(str(action))
+      self._iter = self._iter + 1
+      for g in reward_map.keys():
+        actions.add(g)
+
+    self._iter = 1 
+    self._totalr = 0.0
+    self.terminate()
+    return actions 
+
+  def getState(self, data):
+
+      data = struct.unpack("!i", data)[0]
+      if int(data) != self._iter:
+          print "c++ iter: " + data + " python iter: " + str(self._iter)
+          sys.exit(0)
+      state, reward_map, _ = parse.fileToImage("state.txt", self._iter)
+      return reward_map
+
+  def doAction(self, reward_map):
+      action = reward_map.keys()[0]
+      return action 
+  def terminate(self):
+    self._p.terminate()
+    self._p.wait()
+    print "process finish"
+
+class Gplayer:
+  def __init__(self, sock, idx2regs, regs2idx):
+    self._sock = sock
+    self._sock.listen(5)
+    self._iter = 1
+    self._totalr = 0.0
+    self._idx2Regs = idx2regs
+    self._regs2idx = regs2idx
+    self.best_reward = 0
+    self.process_init = False
+
+  def terprocess(self):
+      print "terminal the process in python"
+      self._p.terminate()
+      self._p.wait()
+
+  def reset(self):
+    self._p = subprocess.Popen(['../llvm-reg/llvm/build/bin/llc', '-debug-only=regallocdl', '--regalloc=drl', 'add.ll', '-o', 'convba.s'],shell=False, stdout=subprocess.PIPE)
+    print "start accept " + str(self._iter)
+    self._iter = 1
+    self._conn, addr = self._sock.accept()
+    data = self._conn.recv(1024)
+    state, reward_map = self.getState(data)
+    return state, reward_map
+
+  def step(self, action):
+    reward = 0.0
+    self._conn.send(str(action))
+    self._iter = self._iter + 1
+    self._conn, addr = self._sock.accept()
+    data = self._conn.recv(1024)
+    if data[0] == 'e':
+        self.terprocess()
+        return [], True, []
+    state, reward_map = self.getState(data)
+    return state, False, reward_map
+
+  def getState(self, data):
+    data = struct.unpack("!i", data)[0]
+    if int(data) != self._iter:
+      print "c++ iter: " + data + " python iter: " + str(self._iter)
+      sys.exit(0)
+    state, reward_map, _ = parse.fileToImage("state.txt", self._iter)
+    return state, reward_map
+
+  def among(self, distri, reward_map, ac):
+    if reward_map.get(str(ac)) != None:
+        reward = reward_map[str(ac)]
+        return int(reward), ac
+    finalidx2reg = {}
+    index = 0
+    actions = [] 
+    for i in self._regs2idx.keys():
+        if reward_map.get(str(i)) != None:
+            actions.insert(index, distri[self._regs2idx[str(i)]])
+            finalidx2reg[str(index)] = i
+            index = index + 1   
+    if random.random() < 0.5:
+      action = np.random.choice(index, 1, actions)
+      action = action[0]
+    else:
+      action = np.argmax(np.array(actions))
+    action = finalidx2reg[str(action)]
+    reward = reward_map[str(action)]
+    return int(reward), action
+
+def among(distri, ac):
+    index = distri.size
+    actions = []
+    for i in range(index):
+        actions.append(distri[0][i])
+    action = np.random.choice(index, 1, actions)
+    return ac
--- train_pg.py
+++ train_pg.py
@@ -88,7 +88,7 @@ def train_PG(
              ):
 
     start = time.time()
-    #env = en.Gplayer(sock, idx2regs, regs2idx)
+    env = en.Gplayer(sock, idx2regs, regs2idx)
 
     # Configure output directory for logging
     logz.configure_output_dir(logdir)
@@ -104,15 +104,15 @@ def train_PG(
     np.random.seed(seed)
 
     # Make the gym environment
-    env = gym.make(env_name)
+    #env = gym.make(env_name)
     
     # Is this env continuous, or discrete?
-    #discrete = True
-    discrete = isinstance(env.action_space, gym.spaces.Discrete)
+    discrete = True
+    #discrete = isinstance(env.action_space, gym.spaces.Discrete)
 
     # Maximum length for episodes
-    max_path_length = max_path_length or env.spec.max_episode_steps
-    #max_path_length = 3333
+    #max_path_length = max_path_length or env.spec.max_episode_steps
+    max_path_length = 3333
 
     #========================================================================================#
     # Notes on notation:
@@ -132,10 +132,10 @@ def train_PG(
     #========================================================================================#
 
     # Observation and action sizes
-    #ob_dim = height * weight 
-    #ac_dim = actionsize
-    ob_dim = env.observation_space.shape[0]
-    ac_dim = env.action_space.n if discrete else env.action_space.shape[0]
+    ob_dim = height * weight 
+    ac_dim = actionsize
+    #ob_dim = env.observation_space.shape[0]
+    #ac_dim = env.action_space.n if discrete else env.action_space.shape[0]
 
     #========================================================================================#
     #                           ----------SECTION 4----------
@@ -204,6 +204,7 @@ def train_PG(
             activation=tf.nn.relu)
 
         sy_sampled_ac = tf.squeeze(tf.multinomial(sy_logits_na, 1), axis=[1]) # Hint: Use the tf.multinomial op
+        sy_soft = tf.nn.softmax(sy_logits_na) # Hint: Use the tf.multinomial op
         sy_logprob_n = tf.nn.sparse_softmax_cross_entropy_with_logits(
             labels=sy_ac_na,
             logits=sy_logits_na)
@@ -281,8 +282,8 @@ def train_PG(
         timesteps_this_batch = 0
         paths = []
         while True:
-            #ob, reward_map = env.reset()
-            ob = env.reset()
+            ob, reward_map = env.reset()
+            #ob = env.reset()
             obs, acs, rewards = [], [], []
             animate_this_episode=(len(paths)==0 and (itr % 10 == 0) and animate)
             steps = 0
@@ -291,13 +292,15 @@ def train_PG(
                     env.render()
                     time.sleep(0.05)
                 obs.append(ob)
-                #[distri] = sess.run(sy_logits_na, feed_dict={sy_ob_no : ob[None]})
-                [ac] = sess.run(sy_sampled_ac, feed_dict={sy_ob_no : ob[None]})
-                #rew, action = env.among(distri, reward_map)
-                acs.append(ac)
-                #ob, done, reward_map = env.step(action)
-                ob, rew, done, _ = env.step(ac)
+                [distri], acc = sess.run([sy_logits_na, sy_sampled_ac], feed_dict={sy_ob_no : ob[None]})
+                #[distri, acc, bution] = sess.run([sy_logits_na, sy_sampled_ac, distribution], feed_dict={sy_ob_no : ob[None]})
+                #ac = en.among(distri, acc[0])
+                rew, action = env.among(distri, reward_map, acc[0])
+                acs.append(action)
                 rewards.append(rew)
+
+                ob, done, reward_map = env.step(action)
+                #ob, rew, done, _ = env.step(ac)
                 steps += 1
                 if done or steps > max_path_length:
                     break
@@ -306,6 +309,7 @@ def train_PG(
                     "action" : np.array(acs)}
             paths.append(path)
             timesteps_this_batch += pathlength(path)
+            print str(timesteps_this_batch) + "go"
             if timesteps_this_batch > min_timesteps_per_batch:
                 break
         total_timesteps += timesteps_this_batch

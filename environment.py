1. in environement, fix the reward function bug and sampling function bug   2. the advantage calculation function fixed

environment.py
@@ -141,7 +141,7 @@ class Gplayer:
         return int(reward), ac, True
     elif valid and reward_map.get(str(ac)) == None:
         ac = self._regs2idx[str(ac)]
-        return 0, ac, False
+        return 0.000001, ac, False
     elif not valid and reward_map.get(str(ac)) != None:
         reward = reward_map[str(ac)]
         ac = self._regs2idx[str(ac)]
@@ -156,7 +156,7 @@ class Gplayer:
             index = index + 1   
     #if random.random() < 0.05:
     actions = softmax(actions)
-    action = np.random.choice(index, 1)
+    action = np.random.choice(index, 1, p=actions)
     action = action[0]
     #else:
     #  actions = softmax(actions)

train_pg.py
@@ -427,8 +427,8 @@ def train_PG(
             # On the next line, implement a trick which is known empirically to reduce variance
             # in policy gradient methods: normalize adv_n to have mean zero and std=1. 
             # YOUR_CODE_HERE
-            adv_n = (adv_n - np.mean(adv_n)) / np.std(adv_n)
-            adv_n = adv_n + 1.0
+            adv_n = (adv_n - np.mean(adv_n)) / (np.std(adv_n) + 1e-8)
+            adv_n = adv_n * (1.0 + 1e-8) + 10.0
 
         #====================================================================================#
         #                           ----------SECTION 5----------